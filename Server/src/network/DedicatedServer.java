4e2a3b044251133c39e8a5d0bfddaec06655a0e3
Nicole Jimenez <jas.jimenez411@gmail.com>
2020-04-04 22:06:20 +0200

    feat(DPOOC2-75): Implement methods for DedicatedServer

--- Server/src/network/DedicatedServer.java
+++ Server/src/network/DedicatedServer.java
@@ -1,4 +1,49 @@
 package network;
 
+import model.entities.TunnelObject;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class DedicatedServer extends Thread {
+    private boolean isOn;
+    private ObjectInputStream objectIn;
+    private ObjectOutputStream objectOut;
+    private Socket sClient;
+
+    public DedicatedServer(Socket sClient) {
+        this.sClient = sClient;
+    }
+
+    public void stopServerConnection() {
+        this.isOn = true;
+        this.start();
+    }
+
+    public void startServerConnection() {
+        this.isOn = false;
+        this.interrupt();
+    }
+
+    public void run() {
+        try {
+            // Create the communication channels
+            objectIn = new ObjectInputStream(sClient.getInputStream());
+            objectOut = new ObjectOutputStream(sClient.getOutputStream());
+
+            while(isOn) {
+                TunnelObject tunnelObject = (TunnelObject) objectIn.readObject();
+
+                // When receiving tunnel object, we need to check its type
+                //if (tunnelObject instanceof AuthenticationInfo) {
+                //    // Handle object
+                //}
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
