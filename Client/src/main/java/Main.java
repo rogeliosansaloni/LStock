121c734ec0aa0c4ac2fa66ae0bba3d93c2f32d69
Nicole Jimenez <jas.jimenez411@gmail.com>
2020-04-07 16:34:12 +0200

    feat: Move JSONReader to Shared utils
    Add Maven support for Shared module and change getConfiguration to
    getClientConfiguration.

--- Client/src/main/java/Main.java
+++ Client/src/main/java/Main.java
@@ -1,11 +1,11 @@
-import model.JSONReader;
+import main.java.utils.JSONReader;
 import network.NetworkConfiguration;
 
 public class Main {
     public static void main (String[] args) {
         //test
         JSONReader jsonReader = new JSONReader();
-        NetworkConfiguration test = jsonReader.getConfiguration();
+        NetworkConfiguration test = jsonReader.getClientConfiguration();
 
     }
 }
