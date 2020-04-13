49668e0b51c9d60eaca38f454953ba47a1595fa1
Kaye Ann Ignacio <kayeann.ignacio@students.salle.url.edu>
2020-04-13 21:01:16 +0200

    Task DPOOC2-82 Added testing for View

--- Client/src/main/java/ClientMain.java
+++ Client/src/main/java/ClientMain.java
@@ -1,15 +1,24 @@
 import java.io.IOException;
 import model.entities.TunnelObject;
+import network.NetworkConfiguration;
 import network.NetworkManager;
+import view.MainView;
+
+import javax.swing.*;
 
 public class ClientMain {
     public static void main (String[] args) {
-        try {
+        /*try {
             // Test for server-client connection
             TunnelObject testOjbect = new TunnelObject();
             NetworkManager.getInstance().sendTunnelObject(testOjbect);
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
+        //test
+        JFrame frame = new JFrame();
+        MainView mainView = new MainView();
+        frame.add(mainView);
+        frame.setVisible(true);
     }
 }
