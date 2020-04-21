6f46a97cc170ddfc8abeddf0511af8c0df7c4a82
Nicole Jimenez <nicolemarie.jimenez@students.salle.url.edu>
2020-04-21 10:34:38 +0200

    fix(DPOOC2-80): Remove tests

--- Server/src/main/java/ServerMain.java
+++ Server/src/main/java/ServerMain.java
@@ -9,14 +9,6 @@ public class ServerMain {
         try {
             Server server = new Server();
             server.startServer();
-
-            ServerConfiguration config = new ServerConfiguration();
-            DBConnector connector = new DBConnector("jdbc:mysql://ec2-15-236-105-133.eu-west-3.compute.amazonaws.com","root","admin","LStock",3306);
-
-            //Intente conectarlo con ServerConfiguration para no hardcodearlo como decis pero no me dejaba conectarme asi que lo puse a mano
-            //DBConnector connector = new DBConnector("",config.getDbUser(),config.getDbPassword(),"LStock",3306);
-            connector.connect();
-
         } catch (IOException e) {
             e.printStackTrace();
         }
