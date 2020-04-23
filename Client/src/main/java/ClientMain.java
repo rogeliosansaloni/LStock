b68963d271f9d9492f224daa2b2425a5bee1f759
rsansaloni <47416577+rsansaloni@users.noreply.github.com>
2020-04-24 00:44:44 +0200

    Task feature/DPOOC2-101: The MainView has been created.

--- Client/src/main/java/ClientMain.java
+++ Client/src/main/java/ClientMain.java
@@ -1,8 +1,11 @@
 import network.NetworkManager;
+import view.MainView;
 
 public class ClientMain {
     public static void main(String[] args) {
         // Start client's connection to the server
-        NetworkManager.getInstance().startServerConnection();
+        //NetworkManager.getInstance().startServerConnection();
+        MainView mainView = new MainView();
+        mainView.setVisible(true);
     }
 }
