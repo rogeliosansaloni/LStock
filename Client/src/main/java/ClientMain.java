280518b715916343c9e45160b487f32bc39bf2ec
rsansaloni <47416577+rsansaloni@users.noreply.github.com>
2020-04-25 11:45:17 +0200

    Task feature/DPOOC2-24: Add branch 24 to remote.

--- Client/src/main/java/ClientMain.java
+++ Client/src/main/java/ClientMain.java
@@ -4,5 +4,6 @@ public class ClientMain {
     public static void main(String[] args) {
         // Start client's connection to the server
         NetworkManager.getInstance().startServerConnection();
+
     }
 }
