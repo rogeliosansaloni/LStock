3df9a096334cfb1ce4c8ac6e4fc2c7a347e5ebc6
Nicole Jimenez <nicole.jimenez@adevinta.com>
2020-04-28 14:15:34 +0200

    feat(DPOOC2-110): Create CompanyDetailController

---
+++ Client/src/main/java/controller/CompanyDetailController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import model.entities.Company;
+import model.entities.User;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CompanyDetailController implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("buyShare")) {
+            // TODO: Handle buying of shares
+        }
+        if (e.getActionCommand().equals("sellShare")) {
+            // TODO: Handle selling of shares
+        }
+    }
+
+    public void updateShares(Company company, User user) {
+
+    }
+}
