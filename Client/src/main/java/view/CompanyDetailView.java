894aafac11c67df1b7f02d2914004125fd2c1c31
Nicole Jimenez <nicole.jimenez@adevinta.com>
2020-04-28 20:43:06 +0200

    feat(DPOOC2-109): Create CompanyDetailView

---
+++ Client/src/main/java/view/CompanyDetailView.java
@@ -0,0 +1,29 @@
+package view;
+
+import model.entities.Company;
+import model.entities.User;
+
+import javax.swing.*;
+
+public class CompanyDetailView {
+    private JButton jbBuy;
+    private JButton jbSell;
+    private JLabel jlActualPrice;
+    private JLabel jlMyShares;
+
+    public CompanyDetailView() {
+
+    }
+
+    public void updateHeader() {
+
+    }
+
+    public void updateShares(Company company, User user) {
+
+    }
+
+    public void showErrorMessages(String error) {
+
+    }
+}
