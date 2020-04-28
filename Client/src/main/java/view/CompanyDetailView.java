df56f2f3c905735448fda338e19e4aca0fe8c7e1
Nicole Jimenez <nicole.jimenez@adevinta.com>
2020-04-28 21:58:34 +0200

    feat(DPOOC2-109): Add JFreeChart

--- Client/src/main/java/view/CompanyDetailView.java
+++ Client/src/main/java/view/CompanyDetailView.java
@@ -2,14 +2,17 @@ package view;
 
 import model.entities.Company;
 import model.entities.User;
+import org.jfree.chart.JFreeChart;
 
 import javax.swing.*;
 
 public class CompanyDetailView {
+    private static final String CANDLESTICK_TITLE = "Company Shares";
     private JButton jbBuy;
     private JButton jbSell;
     private JLabel jlActualPrice;
     private JLabel jlMyShares;
+    private JFreeChart jfcCandleStick;
 
     public CompanyDetailView() {
 
