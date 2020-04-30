c8d11305c342fbff7bb5bbde3d55e7c7ac791cf8
Kaye Ann Ignacio <kayeann.ignacio@students.salle.url.edu>
2020-04-30 11:40:14 +0200

    Task DPOOC2-121 Added UseerProfileInfo attributes

--- Shared/src/main/java/model/entities/UserProfileInfo.java
+++ Shared/src/main/java/model/entities/UserProfileInfo.java
@@ -1,4 +1,42 @@
 package model.entities;
 
-public class UserProfileInfo {
+public class UserProfileInfo extends TunnelObject {
+    private String nickname;
+    private String email;
+    private String description;
+    private float totalBalance;
+
+    public UserProfileInfo() {}
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getTotalBalance() {
+        return totalBalance;
+    }
+
+    public void setTotalBalance(float totalBalance) {
+        this.totalBalance = totalBalance;
+    }
 }
