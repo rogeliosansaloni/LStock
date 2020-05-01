5f55737db09bf7f38b0e33fbf922f14a846b4518
Nicole Jimenez <nicole.jimenez@adevinta.com>
2020-05-01 18:16:09 +0200

    feat(DPOOC2-25): Implement ShareTrade class

--- Shared/src/main/java/model/entities/ShareTrade.java
+++ Shared/src/main/java/model/entities/ShareTrade.java
@@ -1,4 +1,37 @@
 package model.entities;
 
+/**
+ * Represents the DTO that contains the action to do with company shares
+ */
 public class ShareTrade extends TunnelObject {
+    private float sharePrice;
+    private String actionToDo;
+
+    /**
+     * @param sharePrice price of the share
+     * @param actionToDo action to do with the share: buy or sell
+     */
+    public ShareTrade(float sharePrice, String actionToDo){
+        this.sharePrice=sharePrice;
+        this.actionToDo=actionToDo;
+
+    }
+
+
+    // Getters and setters
+    public String getActionToDo() {
+        return actionToDo;
+    }
+
+    public void setActionToDo(String actionToDo) {
+        this.actionToDo = actionToDo;
+    }
+
+    public float getSharePrice() {
+        return sharePrice;
+    }
+
+    public void setSharePrice(float sharePrice) {
+        this.sharePrice = sharePrice;
+    }
 }
