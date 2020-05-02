19fe0d0e19218cf6629e7c59b976b9c75cd0f5be
Nicole Jimenez <nicole.jimenez@adevinta.com>
2020-05-02 02:49:38 +0200

    feat(DPOOC2-124 DPOOC2-123): Create BotMenuView

--- Server/src/main/java/controller/BotMenuController.java
+++ Server/src/main/java/controller/BotMenuController.java
@@ -1,9 +1,16 @@
 package controller;
 
+import view.BotMenuView;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class BotMenuController implements ActionListener {
+    private BotMenuView view;
+
+    public BotMenuController(BotMenuView view) {
+        this.view = view;
+    }
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
 
