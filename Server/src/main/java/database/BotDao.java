b32adaf195f3d718833bf11da73e52843fe3af92
Nicole Jimenez <nicole.jimenez@adevinta.com>
2020-05-02 18:52:33 +0200

    feat(DPOOC2-125): Modify createBot method
    Create bot will insert the new bot. Then it will consult the ddbb to get the
    latest bot by filtering by created_at. It now returns the id of the bot.

--- Server/src/main/java/database/BotDao.java
+++ Server/src/main/java/database/BotDao.java
@@ -20,29 +20,27 @@ public class BotDao {
 
 
     /**
-     * It willl create a bot in the database
-     *
-     * @param bot the bot to be created
+     * Creates a bot associated to a company
+     * @param bot bot to be created
+     * @param company company to which the bot belongs to
+     * @return id of the newly created bot. It will return -1 in case there is an error.
      */
-    public void createBot(Bot bot, Company company) {
-        boolean botExist = false;
-        ResultSet verify = dbConnector.selectQuery("SELECT * FROM Bot WHERE bot_id = " + bot.getBotId() + " AND company_id =" + company.getCompanyId() + ";");
+    public int createBot(Bot bot, Company company) {
+        // Create the new bot for the specified company
+        dbConnector.insertQuery("INSERT INTO Bot (company_id, active_time, probability) " +
+                "VALUES ('" + company.getCompanyId() + "','" + bot.getActiveTime() + "','" + bot.getProbability() + "')");
 
+        // Get the latest bot created for the specified company
+        ResultSet verify = dbConnector.selectQuery("SELECT * FROM Bot WHERE company_id =" + company.getCompanyId() + "" +
+                "ORDER BY created_at DESC LIMIT 1;");
         try {
-            while (verify.next()) {
-
-                System.out.println("This bot already exists.");
-                botExist = true;
-
-            }
-            if (!botExist) {
-                dbConnector.insertQuery("INSERT INTO Bot (bot_id, company_id) " +
-                        "VALUES ('" + bot.getBotId() + "','" + company.getCompanyId() + "','" + bot.getActiveTime() + "','" + bot.getProbability() + "')");
-            }
+            int botId = Integer.parseInt(verify.getObject("bot_id").toString());
+            System.out.println("New bot with id " + botId + "has been created.");
+            return botId;
         } catch (SQLException e) {
-            System.out.println(String.format("Error creating bot for %s", company.getName()));
+            System.out.println(String.format("Error in creating bot for %s", company.getName()));
         }
-
+        return -1;
     }
 
 
@@ -56,10 +54,10 @@ public class BotDao {
             bots = new ArrayList<Bot>();
             while (retrievedBots.next()) {
                 Bot bot = new Bot();
-                bot.setBotId(Integer.getInteger(retrievedBots.getObject("id").toString()));
+                bot.setBotId(Integer.parseInt(retrievedBots.getObject("id").toString()));
                 bot.setActiveTime(Float.parseFloat(retrievedBots.getObject("active_time").toString()));
                 bot.setProbability(Float.parseFloat(retrievedBots.getObject("probability").toString()));
-                bot.setCompany(new Company(Integer.getInteger(retrievedBots.getObject("company_id").toString())));
+                bot.setCompany(new Company(Integer.parseInt(retrievedBots.getObject("company_id").toString())));
                 bots.add(bot);
             }
         } catch (SQLException e) {
