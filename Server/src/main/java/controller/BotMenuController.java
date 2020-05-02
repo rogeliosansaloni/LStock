c290d34256b30558a085a53453db80b509d8068f
Nicole Jimenez <nicole.jimenez@adevinta.com>
2020-05-02 02:42:05 +0200

    feat(DPOOC2-124): Create BotMenuController

---
+++ Server/src/main/java/controller/BotMenuController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class BotMenuController implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+
+    }
+
+    public void validatePercentage(float percentage) {
+        // TODO: Implement validatePercentage
+    }
+}
