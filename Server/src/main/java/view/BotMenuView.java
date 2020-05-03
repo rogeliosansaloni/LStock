5510c46d0630882e3bb12b672a305ec300d43d22
Nicole Jimenez <nicole.jimenez@adevinta.com>
2020-05-03 23:07:02 +0200

    feat: Create views for each menu option
    - DPOOC2-123
    - DPOOC2-127
    - DPOOC2-128
    - DPOOC2-129
    - DPOOC2-130

--- Server/src/main/java/view/BotMenuView.java
+++ Server/src/main/java/view/BotMenuView.java
@@ -1,9 +1,31 @@
 package view;
 
+import utils.StockColors;
+
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class BotMenuView extends JFrame {
+    private static final String TITLE = "StockLS - C2";
+    private static final String CARD_CREATE = "Create Bot";
+    private static final String CARD_EDIT = "Configure Bot";
+    private static final String CARD_REMOVE = "Remove Bot";
+    private static final String CARD_LIST = "Bots";
+    private static final int anchuraPanel = 1080;
+    private static final int alturaPanel = 768;
+    private StockColors color;
+
+    // Main views for each menu option
+    private BotsCreateView botsCreateView;
+    private BotsEditView botsEditView;
+    private BotsRemoveView botsRemoveView;
+    private BotsListView botsListView;
+
+    // Main panels
+    private JPanel jpHeader;
+
+    // Buttons
     private JButton jbCreate;
     private JButton jbRemove;
     private JButton jbShowList;
@@ -11,7 +33,31 @@ public class BotMenuView extends JFrame {
     private JButton jbDisable;
 
     public BotMenuView() {
+        color = new StockColors();
+        this.setTitle(TITLE);
+        this.setSize(anchuraPanel, alturaPanel);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setBackground(color.getBLACK());
+        initViews();
+    }
 
+    private void initViews() {
+        this.botsCreateView = new BotsCreateView();
+        this.botsEditView = new BotsEditView();
+        this.botsRemoveView = new BotsRemoveView();
+        this.botsListView = new BotsListView();
+    }
+
+    public void initUI() {
+        JPanel jpMain = new JPanel();
+        jpMain.setLayout(new BorderLayout());
+        jpMain.setBackground(color.getWHITE());
+
+        // Main header
+        jpHeader = new JPanel(new BorderLayout());
+        jpHeader.setBackground(color.getDarkGreyHeader());
     }
 
     public void registerControllers(ActionListener controller) {
