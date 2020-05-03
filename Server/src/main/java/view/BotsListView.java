4cec108501c38b9243e6e38a7aad2d65728c1af6
Nicole Jimenez <nicole.jimenez@adevinta.com>
2020-05-04 00:58:39 +0200

    feat(DPOOC2-129): Implement attributes and methods

--- Server/src/main/java/view/BotsListView.java
+++ Server/src/main/java/view/BotsListView.java
@@ -1,6 +1,18 @@
 package view;
 
 import javax.swing.*;
+import java.awt.event.ActionListener;
 
 public class BotsListView extends JPanel {
+    private JTable jtBotsList;
+    private JButton jbReturn;
+
+    public BotsListView() {
+
+    }
+
+    public void registerController(ActionListener controller) {
+        this.jbReturn.addActionListener(controller);
+        this.jbReturn.setActionCommand("return");
+    }
 }
