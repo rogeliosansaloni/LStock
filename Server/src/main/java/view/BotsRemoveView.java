85f69c484b6858226364215d61b3b3f6cb126a81
Nicole Jimenez <nicole.jimenez@adevinta.com>
2020-05-04 01:04:18 +0200

    feat(DPOOC2-129): Implement attributes and methods

--- Server/src/main/java/view/BotsRemoveView.java
+++ Server/src/main/java/view/BotsRemoveView.java
@@ -1,6 +1,27 @@
 package view;
 
 import javax.swing.*;
+import java.awt.event.ActionListener;
 
 public class BotsRemoveView extends JPanel {
+    private JComboBox<String> jcbBot;
+    private JComboBox<String> jcbCompany;
+    private JLabel jlStatus;
+    private JButton jbRemove;
+    private JButton jbCancel;
+
+    public BotsRemoveView() {
+
+    }
+
+    public void registerController(ActionListener controller) {
+        this.jbRemove.addActionListener(controller);
+        this.jbRemove.setActionCommand("removeExistingBot");
+        this.jbCancel.addActionListener(controller);
+        this.jbCancel.setActionCommand("cancelBotRemoval");
+    }
+
+    public String getBotName() {
+        return jcbBot.getSelectedItem().toString();
+    }
 }
