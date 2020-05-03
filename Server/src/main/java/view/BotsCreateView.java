7ba178c087831a367e0e2db9ea3b9d07213ec333
Nicole Jimenez <nicole.jimenez@adevinta.com>
2020-05-04 00:06:47 +0200

    feat(DPOOC2-128): Implement BotsCreateView
    Implement methods to get information from the view

--- Server/src/main/java/view/BotsCreateView.java
+++ Server/src/main/java/view/BotsCreateView.java
@@ -1,6 +1,40 @@
 package view;
 
 import javax.swing.*;
+import java.awt.event.ActionListener;
 
 public class BotsCreateView extends JPanel {
+    private JTextField jtfName;
+    private JComboBox<String> jcbCompany;
+    private JTextField jtfPercentage;
+    private JTextField jtfActivation;
+    private JButton jbCreate;
+    private JButton jbCancel;
+
+    public BotsCreateView() {
+
+    }
+
+    public void registerController(ActionListener controller) {
+        this.jbCreate.addActionListener(controller);
+        this.jbCreate.setActionCommand("createNewBot");
+        this.jbCancel.addActionListener(controller);
+        this.jbCancel.setActionCommand("cancelCreate");
+    }
+
+    public String getBotName() {
+        return jtfName.getText();
+    }
+
+    public String getCompanyMame() {
+        return jtfName.getText();
+    }
+
+    public float getPercentage() {
+        return Float.parseFloat(jtfPercentage.getText());
+    }
+
+    public float getActivation() {
+        return Float.parseFloat(jtfActivation.getText());
+    }
 }
