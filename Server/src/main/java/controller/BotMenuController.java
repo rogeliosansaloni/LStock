4bef6ac7ef522475f0ba989894d39af32d916a01
Nicole Jimenez <nicole.jimenez@adevinta.com>
2020-05-03 23:07:40 +0200

    feat(DPOOC2-124): Structure for each option for bot menu

--- Server/src/main/java/controller/BotMenuController.java
+++ Server/src/main/java/controller/BotMenuController.java
@@ -1,19 +1,37 @@
 package controller;
 
-import view.BotMenuView;
+import model.managers.BotManager;
+import view.*;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class BotMenuController implements ActionListener {
     private BotMenuView view;
+    private BotManager model;
 
-    public BotMenuController(BotMenuView view) {
+    public BotMenuController(BotMenuView view, BotManager model) {
         this.view = view;
+        this.model = model;
     }
-    @Override
-    public void actionPerformed(ActionEvent actionEvent) {
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String mode = e.getActionCommand();
+        switch (mode) {
+            case "createBot":
+                // TODO: Handle bot creation
+                break;
+            case "editBot":
+                // TODO: Handle bot update
+                break;
+            case "deleteBot":
+                // TODO: Handke bot deletion
+                break;
+            case "listBots":
+                // TODO: Handle bot list
+                break;
+        }
     }
 
     public void validatePercentage(float percentage) {
