0e71d54cd4508a6fd0ff50d0aed5c4dfeee94419
Nicole Jimenez <nicole.jimenez@adevinta.com>
2020-05-04 00:57:07 +0200

    feat(DPOOC2-130): Implement attributes and methods

--- Server/src/main/java/view/BotsEditView.java
+++ Server/src/main/java/view/BotsEditView.java
@@ -1,6 +1,30 @@
 package view;
 
 import javax.swing.*;
+import java.awt.event.ActionListener;
 
 public class BotsEditView extends JPanel {
+    private JComboBox<String> jcbBot;
+    private JComboBox<String> jcbCompany;
+    private JButton jbActivate;
+    private JButton jbDeactivate;
+
+    public BotsEditView() {
+
+    }
+
+    public String getBotName() {
+        return jcbBot.getSelectedItem().toString();
+    }
+
+    public String getCompany() {
+        return jcbCompany.getSelectedItem().toString();
+    }
+
+    public void registerController(ActionListener controller) {
+        this.jbActivate.addActionListener(controller);
+        this.jbActivate.setActionCommand("activateBot");
+        this.jbDeactivate.addActionListener(controller);
+        this.jbDeactivate.setActionCommand("deactivateBot");
+    }
 }
