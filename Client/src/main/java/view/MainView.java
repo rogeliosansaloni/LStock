511c9d01ee1d5aa6cdad0ed7a6d90059796192d5
Kaye Ann Ignacio <kayeann.ignacio@students.salle.url.edu>
2020-05-04 21:36:46 +0200

    Changed name

--- Client/src/main/java/view/MainView.java
+++ Client/src/main/java/view/MainView.java
@@ -210,7 +210,7 @@ public class MainView extends JFrame {
      *
      * @param actionListener ActionLister
      */
-    public void registerController(ActionListener actionListener) {
+    public void registerMainController(ActionListener actionListener) {
         option1.addActionListener(actionListener);
         option1.setActionCommand("profile");
         option2.addActionListener(actionListener);
