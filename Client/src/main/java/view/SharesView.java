d0527ffc6003c36380a4675ac9a0e6aec7906135
claudia <claudia.alonso@students.salle.url.edu>
2020-05-04 12:15:30 +0200

    DPOOC2-104 - create sharesview

---
+++ Client/src/main/java/view/SharesView.java
@@ -0,0 +1,15 @@
+package view;
+
+import utils.StockColors;
+
+import javax.swing.*;
+
+public class SharesView extends JPanel {
+    private JTable tableShares;
+    private JPanel jpCenter;
+    private JPanel jpTable;
+    protected StockColors color;
+
+
+
+}
