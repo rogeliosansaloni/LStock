2cad12eb1ad6094c2d806e6c4a80e9d07a4e6125
Nicole Jimenez <nicole.jimenez@adevinta.com>
2020-05-05 01:37:31 +0200

    feat(DPOOC2-103): Retrieve all companies from the database

--- Client/src/main/java/model/entities/StockManager.java
+++ Client/src/main/java/model/entities/StockManager.java
@@ -1,7 +1,10 @@
 package model.entities;
 
+import java.util.ArrayList;
+
 public class StockManager {
     private User user;
+    private ArrayList<Company> companies;
 
     public StockManager(User user) {
         this.user = user;
@@ -9,5 +12,17 @@ public class StockManager {
 
     public User getUser() {
         return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public ArrayList<Company> getCompanies() {
+        return companies;
+    }
+
+    public void setCompanies(ArrayList<Company> companies) {
+        this.companies = companies;
     }
 }
