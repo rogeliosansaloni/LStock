c11d2ce9bd3dc3b7492e1d6b8408581da47fe362
Nicole Jimenez <nicole.jimenez@adevinta.com>
2020-05-07 10:41:59 +0200

    fix(DPOOC2-104): Fix errors in SharesView

--- Client/src/main/java/view/SharesView.java
+++ Client/src/main/java/view/SharesView.java
@@ -10,9 +10,9 @@ public class SharesView extends JPanel {
     private JPanel jpCenter;
     private JPanel jpTable;
     private JButton[][] jlCompanies;
-    private String[] companiesNames = {"Company 1", "Company 2", "Company 3", "Company 4"};;
+    private String[] companiesNames = {"Company 1", "Company 2", "Company 3", "Company 4"};
     private JButton[][] jlSell;
-    private String[] sellNames = {"Sell All Shares 1", "Sell All Shares 2", "Sell All Shares 3", "Sell All Shares 4"};;
+    private String[] sellNames = {"Sell All Shares 1", "Sell All Shares 2", "Sell All Shares 3", "Sell All Shares 4"};
     protected StockColors color;
 
     public SharesView() {
@@ -22,7 +22,7 @@ public class SharesView extends JPanel {
         jpTable = new JPanel();
         jpTable.setLayout(new GridLayout(5, 5, 20, 20));
 
-        for(int i=0; i<5; i++){
+        for (int i = 0; i < 5; i++) {
             createColumnLabel("COMPANY");
             createColumnLabel("PRICE 1");
             createColumnLabel("CHANGE (5 min)");
@@ -31,46 +31,48 @@ public class SharesView extends JPanel {
             jlSell = new JButton[sellNames.length][4];
 
             //We create a row for each company available.
-            for(int i=0; i<companiesNames.length; i++) {
-                creatDataLabel("COMPANY " + (i + 1), color.getWHITE(), i, 0);
-                creatDataLabel((i + 1) * 100 + "€", color.getGreenTable(), i, 1);
-                creatDataLabel((i + 1) * 30 + "€", color.getRedTable(), i, 2);
-                creatDataLabel("0.5%", color.getGreenTable(), i, 3);
-                createDataLabelSmall("Sell All Shares", color.getWHITE(), i, 4);
+            for (int j = 0; j < companiesNames.length; j++) {
+                creatDataLabel("COMPANY " + (j + 1), color.getWHITE(), j, 0);
+                creatDataLabel((j + 1) * 100 + "€", color.getGreenTable(), j, 1);
+                creatDataLabel((j + 1) * 30 + "€", color.getRedTable(), j, 2);
+                creatDataLabel("0.5%", color.getGreenTable(), j, 3);
+                createDataLabelSmall("Sell All Shares", color.getWHITE(), j, 4);
 
             }
 
             jpTable.setBackground(color.getBLACK());
             this.add(jpTable, BorderLayout.CENTER);
-            this.setBorder(BorderFactory.createEmptyBorder(50,50,50,50));
+            this.setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50));
             this.setBackground(color.getBLACK());
 
         }
-
-        /**
-         * Adds actionListener to load button
-         * @param actionListener ActionListener
-         */
-        public void registerController(ActionListener actionListener) {
-            //We add an actionListener for each company
-            for(int i=0; i<jlCompanies.length; i++){
-                for(int j=0; j<4; j++){
-                    jlCompanies[i][j].addActionListener(actionListener);
-                    jlCompanies[i][j].setActionCommand(companiesNames[i]);
-                }
-            }
-            //We add an actionListener for each sell shares button
-            for(int i=0; i<jlSell.length; i++){
-                for(int j=0; j<4; j++){
-                    jlSell[i][j].addActionListener(actionListener);
-                    jlSell[i][j].setActionCommand(sellNames[i]);
-                }
-            }
-        }
-
     }
 
-    public void createColumnLabel(String text){
+    /**
+     * Adds actionListener to load button
+     *
+     * @param actionListener ActionListener
+     */
+    public void registerController(ActionListener actionListener) {
+        //We add an actionListener for each company
+        for (int i = 0; i < jlCompanies.length; i++) {
+            for (int j = 0; j < 4; j++) {
+                jlCompanies[i][j].addActionListener(actionListener);
+                jlCompanies[i][j].setActionCommand(companiesNames[i]);
+            }
+        }
+        //We add an actionListener for each sell shares button
+        for (int i = 0; i < jlSell.length; i++) {
+            for (int j = 0; j < 4; j++) {
+                jlSell[i][j].addActionListener(actionListener);
+                jlSell[i][j].setActionCommand(sellNames[i]);
+            }
+        }
+    }
+
+
+
+    public void createColumnLabel(String text) {
         JLabel label = new JLabel(text);
         label.setHorizontalAlignment(SwingConstants.CENTER);
         label.setBackground(color.getWHITE());
@@ -81,13 +83,13 @@ public class SharesView extends JPanel {
         jpTable.add(label);
     }
 
-    public void creatDataLabel(String text, Color c, int i, int j){
+    public void creatDataLabel(String text, Color c, int i, int j) {
         jlCompanies[i][j] = new JButton(text);
         jlCompanies[i][j].setHorizontalAlignment(SwingConstants.CENTER);
         jlCompanies[i][j].setBackground(c);
-        if(c == color.getWHITE()){
+        if (c == color.getWHITE()) {
             jlCompanies[i][j].setForeground(color.getBLACK());
-        } else{
+        } else {
             jlCompanies[i][j].setForeground(color.getWHITE());
         }
         jlCompanies[i][j].setBorderPainted(false);
@@ -95,7 +97,8 @@ public class SharesView extends JPanel {
         jlCompanies[i][j].setFont(font);
         jpTable.add(jlCompanies[i][j]);
     }
-    public void createDataLabelSmall(String text, Color c, int i, int j){
+
+    public void createDataLabelSmall(String text, Color c, int i, int j) {
         jlSell[i][j] = new JButton(text);
         jlSell[i][j].setBackground(color.getWHITE());
         jlSell[i][j].setPreferredSize(new Dimension(170, 40));
