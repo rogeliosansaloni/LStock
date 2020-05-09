868b8c95f5e99096399d40e56b99f5529bfbb50a
claudia <claudia.alonso@students.salle.url.edu>
2020-05-09 16:34:37 +0200

    DPOOC2-104 - add CompanyChange Class in Server

---
+++ Server/src/main/java/model/entities/CompanyChange.java
@@ -0,0 +1,51 @@
+package model.entities;
+
+
+public class CompanyChange {
+    private String name;
+    private float currentShare;
+    private float change;
+    private float changePer;
+
+    public CompanyChange() {
+    }
+
+    public CompanyChange(String name, float currentShare, float change, float changePer) {
+        this.name = name;
+        this.currentShare = currentShare;
+        this.change = change;
+        this.changePer = changePer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getCurrentShare() {
+        return currentShare;
+    }
+
+    public void setCurrentShare(float currentShare) {
+        this.currentShare = currentShare;
+    }
+
+    public float getChange() {
+        return change;
+    }
+
+    public void setChange(float change) {
+        this.change = change;
+    }
+
+    public float getChangePer() {
+        return changePer;
+    }
+
+    public void setChangePer(float changePer) {
+        this.changePer = changePer;
+    }
+}
