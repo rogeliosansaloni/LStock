40ac9425fc5f18fdeb5feef094ad3a1a7729bf6a
claudia <claudia.alonso@students.salle.url.edu>
2020-05-16 19:12:30 +0200

    DPOOC2-104 - add ShareChange to server

---
+++ Server/src/main/java/model/entities/ShareChange.java
@@ -0,0 +1,60 @@
+package model.entities;
+
+public class ShareChange {
+
+    private String name;
+    private float actionValue;
+    private float myActions;
+    private float profitLoss;
+
+    public ShareChange() {
+
+    }
+
+    public ShareChange(String name, float actionValue, float myActions, float profitLoss) {
+        this.name = name;
+        this.actionValue = actionValue ;
+        this.myActions = myActions;
+        this.profitLoss = profitLoss;
+    }
+
+    public ShareChange(int i, String name, float v, float myAction, float profitLoss) {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getActionValue() {
+        return actionValue;
+    }
+
+    public void setActionValue(float actionValue) {
+        this.actionValue = actionValue;
+    }
+
+    public float getMyActions() {
+        return myActions;
+    }
+
+    public void setMyActions(float myActions) {
+        this.myActions = myActions;
+    }
+
+    public float getProfitLoss() {
+        return profitLoss;
+    }
+
+    public void setProfitLoss(float profitLoss) {
+        this.profitLoss = profitLoss;
+    }
+
+    public int getCompanyId() {
+        // TODO: Get company id of which we want to sell the shares
+        return 1;
+    }
+}
