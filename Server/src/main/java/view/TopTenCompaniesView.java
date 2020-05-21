2376747a59f12b334f6bfeaa4ecfb5127a24c4e9
Mary Grace <marygrace.adina@students.salle.url.edu>
2020-05-21 21:29:05 +0200

    feature/DPOOC2-185 Initial view

--- Server/src/main/java/view/TopTenCompaniesView.java
+++ Server/src/main/java/view/TopTenCompaniesView.java
@@ -1,113 +1,37 @@
 package view;
 
+
+import utils.StockColors;
+
+import javax.swing.*;
+import javax.swing.plaf.basic.BasicComboBoxUI;
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.Toolkit.*;
-import javax.swing.*;
-import java.io.*;
-import java.util.*;
-
-import javax.swing.*;
-
+import java.util.ArrayList;
 public class TopTenCompaniesView extends JPanel {
+    private static final String FONT = "Segoe UI";
+    private static final String FONT_BUTTON = "Segoe UI Semibold";
+    private static final String TITLE = "Top 10 Companies";
+    private static final String CREATE = "SHARE";
+    private static final int WIDTH = 200;
+    private static final int HEIGHT = 20;
+    private JButton jbShare;
 
-        private final int OUTER_MARGIN = 20;
-        private static final Color BACKGROUND_COLOR = Color.white;
-        private static final Color BAR_COLOR = Color.red;
-        private int SPACE_ON_LEFT_RIGHT;
-        private Image fImageBuffer;
-        private Insets fInsets;
-        private Graphics g;
-        private Bar[] bars;
-        private static int SLEEP = 500;
-        private int[] inputData;
+    public TopTenCompaniesView(){
+        StockColors color = new StockColors();
+        Font font = new Font(FONT, Font.ITALIC, 20);
 
+        this.setLayout(new GridLayout(6,1,0,15));
+        this.setBackground(color.getWHITE());
 
-        class Bar
-        {
-            private int height, value;
-            public int width;
-            public int nwx;
-            public int nwy;
-            public Color color;
-
-            public Bar() {}
-            public Bar(int height, int width, int value, int nwx, int nwy)
-            {
-                this.height = height;
-                this.width = width;
-                this.value = value;
-                this.nwx = nwx;
-                this.nwy = nwy;
-            }
-
-        }
+        JLabel jlTitle = new JLabel(TITLE);
+        jlTitle.setPreferredSize(new Dimension(200, 1));
+        jlTitle.setHorizontalAlignment(JLabel.CENTER);
+        jlTitle.setFont(font);
+        this.add(jlTitle);
 
 
-      /*  public SimpleBarchart(final int[] inputData)
-        {
-            this.inputData = inputData;
-            addWindowListener(new WindowCloser());
-            fInsets = getInsets();
-            setSize(WIDTH + fInsets.left + fInsets.right, HEIGHT + fInsets.top + fInsets.bottom);
-            setTitle("Bar Chart");
-            if (((fImageBuffer = createImage(WIDTH, HEIGHT)) == null) ||
-                    ((g = fImageBuffer.getGraphics()) == null))
-                System.exit(1);
-            readData();
-            createBars();
-            getContentPane().add(new SimpleBarchart(inputData), BorderLayout.CENTER);
-            setVisible(true);
-        }*/
-
-        /**
-         *
-         * @param g
-         */
-        /*protected void paintComponent(final Graphics g) {
-            g.drawImage(fImageBuffer, fInsets.left, fInsets.top, null);
-        }*/
-
-
-        private void readData()
-        {
-            String[] inputItems = JOptionPane.showInputDialog("Enter 1 to 9 integers > 0").trim().split(" +");
-            int numData = inputItems.length;
-            inputData = new int[numData];
-
-            for (int itemIndex = 0; itemIndex < inputItems.length; itemIndex++)
-                inputData[itemIndex] = numData;
-
-        }
-
-        private void createBars()
-        {
-            Bar[] bars = new Bar[2];
-            int pixelBetweenBars = 25;
-            int width = 100 + 2*OUTER_MARGIN;
-            int height = 100 + 2*OUTER_MARGIN;
-
-        }
-
-        private void drawBars(final Graphics g)
-        {
-            int OUTER_MARGIN = 20,
-                    WIDTH = 800 + 2 * OUTER_MARGIN,
-                    HEIGHT = 600 + 2 * OUTER_MARGIN;
-
-
-            g.setColor(BACKGROUND_COLOR);
-            g.fillRect(0, 0, WIDTH, HEIGHT);
-
-            g.setColor(BAR_COLOR);
-            final int barWidth = 20;
-            for (int itemIndex = 0; itemIndex < inputData.length; itemIndex++) {
-                final int x = OUTER_MARGIN + 25 * itemIndex;
-                final int barHeight = 10 * inputData[itemIndex];
-                final int y = barHeight;
-                g.fillRect(x, y, barWidth, barHeight);
-            }
-        }
+    }
 
 
 }
