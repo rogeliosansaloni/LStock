4924717188453e7c4aec8822e9e48eeea111f6c9
Nicole Jimenez <jas.jimenez411@gmail.com>
2020-05-23 15:11:09 +0200

    fix(DPOOC2-171): Remove extra space

--- Client/src/main/java/controller/MainController.java
+++ Client/src/main/java/controller/MainController.java
@@ -112,5 +112,4 @@ public class MainController implements ActionListener {
         //TODO: Update company in the model
         companyDetailController.updateCompanyUserValueAndBalance(totalBalance, value);
     }
-
 }
